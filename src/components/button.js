e2b880bf11ae8dbbadbcb1047106560849ca8c55
Matthew Warner <mattwarner14@yahoo.com>
2018-06-28 15:02:40 -0600

    created functional and corisponding styles

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+const Button = (title) => {
+    return (
+        <button className="button">
+             {title}
+        </button>    
+    )
+} 
+export default Button;

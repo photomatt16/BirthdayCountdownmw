38a5166d5bef468c6d695e428838abb0a4af8fe3
Matthew Warner <mattwarner14@yahoo.com>
2018-06-29 13:58:36 -0600

    added large text and remaining label. finished css front end

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,11 @@ import Picker from './picker';
 import Button from './button';
 import Clock from './clock';
 import ChangeDate from './changeDate';
+import LargeText from './largeText';
 
 export default class App extends Component {
 
+  
   constructor(props) {
     super(props)
     this.state = {
@@ -13,22 +15,24 @@ export default class App extends Component {
     }
   }
 
+  
   renderItems= function() {
     if(this.state.active) {
       return [
         <Clock/>,
-        ChangeDate('Change Date', () => this.setState({ active: false}))
+        ChangeDate('Change Date', () => this.setState({ active: false})), 
+        LargeText('04/03'),
+        <label className="grid__remaining">Remaining untill your 21st birthday</label>
       ]
     } else {
-      return Button('Generate Countdown', () => this.setState({ active: true})) 
+      return [
+        <Picker/>, 
+        Button('Generate Countdown', () => this.setState({ active: true})) 
+      ]
     }
-
   }.bind(this)
 
   render() {
-
-    // return <div className=""><Clock/></div>
-
     return (
       <div className="grid">
         <h1 className="grid__title">Birthday Countdown</h1>
@@ -41,7 +45,6 @@ export default class App extends Component {
         <div className="grid__skew-light-two"></div>
         <div className="grid__skew-light-three-box"></div>
 
-        <Picker/>
         { this.renderItems() }
 
       </div>
---
+++ src/components/largeText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const LargeText = (text) => {
+    return(
+        <div className="large-text">
+            {text}
+        </div>
+    )
+}
+
+export default LargeText

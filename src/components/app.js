4a278f740e93dc7262927e4f78c952882d66c1e0
Matthew Warner <mattwarner14@yahoo.com>
2018-06-29 09:33:33 -0600

    styled clock with CSS GRid and detailed styles

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,12 @@ import Clock from './clock';
 
 export default class App extends Component {
   render() {
+
+    // return <div className=""><Clock/></div>
+
     return (
       <div className="grid">
-        <h1 className="grid__title">BIrthday Countdown</h1>
+        <h1 className="grid__title">Birthday Countdown</h1>
        
         <div className="grid__skew-dark-one-box"></div>
         <div className="grid__skew-dark-two"></div>

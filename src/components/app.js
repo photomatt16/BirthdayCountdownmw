cb7b713e5ee19566d3fe0b8257187074577cb1d0
Matthew Warner <mattwarner14@yahoo.com>
2018-06-29 22:25:11 -0600

    fixed misspelling  buggs

--- src/components/app.js
+++ src/components/app.js
@@ -11,7 +11,7 @@ import moment from 'moment';
 export default class App extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.timer = 0
 
@@ -51,21 +51,21 @@ export default class App extends Component {
     
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
     const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    const timerRemaining = {
-
+    const timeRemaining = {
       days,
       hours,
       minutes,
       seconds
     };
-
-    this.setState({ timeRrmaining });
+    this.setState({ timeRemaining });
 
     // If the count down is over, write some text 
     if (distance < 0) {
@@ -87,7 +87,10 @@ export default class App extends Component {
       ];
     } else {
       return [
-        <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
+        <Picker 
+        startDate={this.state.startDate} 
+        callback={date => this.handleChange(date)}
+        />,
         Button('Generate Countdown', () => this.handleGenerate())
       ]
     }

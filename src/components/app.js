8af30b98e4653371b9f160b07d5f66b54050fea8
Matthew Warner <mattwarner14@yahoo.com>
2018-06-29 21:30:00 -0600

    cleaned up picker js

--- src/components/app.js
+++ src/components/app.js
@@ -15,7 +15,13 @@ export default class App extends Component {
 
     this.state = {
       active: false,
-      startDate: moment()
+      startDate: moment(),
+      timeRemaining: {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+      }
     }
   }
 
@@ -50,6 +56,16 @@ export default class App extends Component {
     // Output the result in an element with id="demo"
     const time = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
     console.log(time)
+    this.setState({
+      timeREmaining: {
+        days,
+        hours,
+        minutes,
+        seconds
+      }
+    })
+
+    console.log(this.state.timeREmaining);
 
     // If the count down is over, write some text 
     if (distance < 0) {
@@ -69,7 +85,7 @@ export default class App extends Component {
       ]
     } else {
       return [
-        <Picker callback={(date) => this.handleChange(date)}/>,
+        <Picker startDate={this.state.startDate} callback={(date) => this.handleChange(date)}/>,
         Button('Generate Countdown', () => this.handleGenerate())
       ]
     }
